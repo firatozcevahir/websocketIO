be3afc0611ca0dde3a545701d0aacf318ec65cfe
fırat özcevahir <firat.ozcevahir@gmail.com>
2019-10-18 11:39:34 +0300

    Websocket IO
    -Added comment lines to site.js

--- WebSocketProject/wwwroot/js/site.js
+++ WebSocketProject/wwwroot/js/site.js
@@ -1,23 +1,45 @@
-﻿//default websocket url
-var url = "ws://192.168.43.159:81";
-//var url = "ws://10.34.82.5:81";
+﻿/* 
+ * Websocket project
+ * 
+ * 
+ * required data attributes of checkboxes for the switches  : data-module-name, data-io-mode, data-io-number
+ * each checkbox must have 'chck-switch' class
+ * each checkbox must be within a div with id targeted by related tab navigation button
+ * 
+ * to get the switches in current area, each tab navigation button must have 'btn-area' class
+ * required data attributes for navigation buttons: data-socket-ip, data-target-area
+ * each button must target a div that switches are in * 
+ * 
+ * 
+ */
 
 var ws = null;
 var commandText = "";
 var processedCommand = {};
 
-var switches = $("#area1").find(".chck-switch");
+//default websocket url from the default area on first load
+var url = "ws://" + $(".btn-area:first").data("socket-ip");
+//default switches from the default area on first load
+var switches = $("#" + $(".btn-area:first").data("target-area")).find(".chck-switch");
+
 var btnAreas = $(".btn-area");
 var chckToggleAll = $("#chck_toggleall");
 var overlay = $("#overlay");
 
 $(document).ready(function () {
+    //create the first websocket with default configuration
     createWebSocket();
 });
 
 btnAreas.click(function (e) {
-    url = "ws://" + $(e.target).data('socket-ip') + ":81";
-    switches = $("#" + $(e.target).data('target-area')).find(".chck-switch");
+
+    $(btnAreas).parent().removeClass("p-event-none");
+    $(e.target).parent().addClass("p-event-none");
+
+    //get the websocket ip from the related object
+    url = "ws://" + $(e.target).data("socket-ip");
+    //get the switches of selected area using the data attribute of the related object
+    switches = $("#" + $(e.target).data("target-area")).find(".chck-switch");
     overlay.show();
     ws = null;
     createWebSocket();
@@ -33,19 +55,19 @@ function createWebSocket() {
 
 var onOpen = function () {    
     switches.change(function (e) {
-        //get the command string of related switch object and send to the websocket
+        //get the command string of related switch object and send via websocket
         commandText = getCommandText(e.target, "cmd");
         ws.send(commandText);
     });
 
-    //first states of all the switches will be send from here on connection open
+    //first state of each switch in the related area will be send from here on connection open
     switches.each(function () {
         commandText = getCommandText(this, "req");
         ws.send(commandText);
     });
     overlay.fadeOut(1000);
 
-    console.log('websocket connected to: ' + url);
+    console.log("websocket connected to: " + url);
 }
 
 var onMessage = function (event) {
@@ -59,7 +81,7 @@ var onMessage = function (event) {
 
 var onError = function (event) {
     //websocket connection fail
-    console.log('websocket failed to connect: ' + url);
+    console.log("websocket failed to connect: " + url);
     setTimeout("location.reload(true);", 1000);
 }
 
@@ -80,17 +102,17 @@ function processCommand(cmdText) {
 }
 
 function getCommandText(e, type) {
-    //return the command type and data attribute values of the related object as a command string
-    return type + $(e).data('module-name') + $(e).data('io-mode') + $(e).data('io-number') + (e.checked ? "01" : "00");
+    //return the command type and data attribute values of the related switch as a command string
+    return type + $(e).data("module-name") + $(e).data("io-mode") + $(e).data("io-number") + (e.checked ? "01" : "00");
 }
 
 function updateStateOfSwitch(pcommand) {
-    //change the 'checked' state of the related object depending on the processed command
+    //change the 'checked' state of the related switch depending on the processed command
     $('[data-module-name="' + pcommand.modulename + '"][data-io-mode="' + pcommand.iomode + '"][data-io-number="' + pcommand.ionumber + '"]').prop('checked', (pcommand.state == "01" ? true : false));
 }
 
 chckToggleAll.change(function (e) {
-    //change the 'checked' state of all the switches depending on the related object's 'checked' state
+    //change the 'checked' state of each switch in the current area depending on the related object's 'checked' state
     switches.each(function () {
         this.checked = e.target.checked;
         commandText = getCommandText(this, "cmd");
